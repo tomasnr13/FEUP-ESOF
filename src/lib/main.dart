05d7569c406d30a2e6263433c8f840d3a68f3254
Lucas Calvet Santos <lucascalvet2000@hotmail.com>
2022-04-19 10:08:35 +0100

    Imported UNI code

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,233 +1,118 @@
-import 'dart:developer';
+import 'dart:async';
 
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:sentry/sentry.dart';
+import 'package:redux/redux.dart';
+import 'package:uni/controller/middleware.dart';
+import 'package:uni/model/app_state.dart';
+import 'package:uni/redux/actions.dart';
+import 'package:uni/redux/reducers.dart';
+import 'package:uni/utils/constants.dart' as Constants;
+import 'package:uni/view/Pages/about_page_view.dart';
+import 'package:uni/view/Pages/bug_report_page_view.dart';
+import 'package:uni/view/Pages/bus_stop_next_arrivals_page.dart';
+import 'package:uni/view/Pages/exams_page_view.dart';
+import 'package:uni/view/Pages/home_page_view.dart';
+import 'package:uni/view/Pages/logout_route.dart';
+import 'package:uni/view/Pages/splash_page_view.dart';
+import 'package:uni/view/Widgets/page_transition.dart';
+import 'package:uni/view/navigation_service.dart';
+import 'package:uni/view/theme.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'controller/on_start_up.dart';
+import 'model/schedule_page_model.dart';
+
+/// Stores the state of the app
+final Store<AppState> state = Store<AppState>(appReducers,
+    /* Function defined in the reducers file */
+    initialState: AppState(null),
+    middleware: [generalMiddleware]);
+
+SentryEvent beforeSend(SentryEvent event) {
+  return event.level == SentryLevel.info ? event : null;
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+Future<void> main() async {
+  OnStartUp.onStart(state);
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://a2661645df1c4992b24161010c5e0ecb@o553498.ingest.sentry.io/5680848';
+    },
+    appRunner: () => {runApp(MyApp())},
+  );
+}
 
-  // This widget is the root of your application.
+/// Manages the state of the app
+/// 
+/// This class is necessary to track the app's state for
+/// the current execution
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'UNI Workluster',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(title: 'UNI Workluster'),
-    );
+  State<StatefulWidget> createState() {
+    return MyAppState(
+        state: Store<AppState>(appReducers,
+            /* Function defined in the reducers file */
+            initialState: AppState(null),
+            middleware: [generalMiddleware]));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+/// Manages the app depending on its current state
+class MyAppState extends State<MyApp> {
+  MyAppState({@required this.state}) {}
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  void _sendEmail(String _email) async {
-    try {
-      launch('emailto:$_email');
-    } catch (e) {
-      log('Could not email to $_email. ' + e.toString());
-    }
-  }
-
-  Future<void> _showInfo() async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: true,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('App Info'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                const Text(
-                    'This app is still under development. With Workluster, students will be able to easily form groups for course projects.\n',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                const Text('Developed by:'),
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      const TextSpan(
-                        text: '\u2022 Francisco Pires (',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      TextSpan(
-                        text: 'up201908044@fe.up.pt',
-                        style: const TextStyle(color: Colors.blue),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            _sendEmail('up201908044@fe.up.pt');
-                          },
-                      ),
-                      const TextSpan(
-                        text: ')\n',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      const TextSpan(
-                        text: '\u2022 José Pedro Ferreira (',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      TextSpan(
-                        text: 'up201904515@fe.up.pt',
-                        style: const TextStyle(color: Colors.blue),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            _sendEmail('up201904515@fe.up.pt');
-                          },
-                      ),
-                      const TextSpan(
-                        text: ')\n',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      const TextSpan(
-                        text: '\u2022 Lucas Santos (',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      TextSpan(
-                        text: 'up201904517@fe.up.pt',
-                        style: const TextStyle(color: Colors.blue),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            _sendEmail('up201904517@fe.up.pt');
-                          },
-                      ),
-                      const TextSpan(
-                        text: ')\n',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      const TextSpan(
-                        text: '\u2022 Sérgio Gama (',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      TextSpan(
-                        text: 'up201906690@fe.up.pt',
-                        style: const TextStyle(color: Colors.blue),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            _sendEmail('up201906690@fe.up.pt');
-                          },
-                      ),
-                      const TextSpan(
-                        text: ')\n',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      const TextSpan(
-                        text: '\u2022 Tomás Fidalgo (',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      TextSpan(
-                        text: 'up201906743@fe.up.pt',
-                        style: const TextStyle(color: Colors.blue),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            _sendEmail('up201906743@fe.up.pt');
-                          },
-                      ),
-                      const TextSpan(
-                        text: ')\n',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Close'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
+  final Store<AppState> state;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-        backgroundColor: const Color.fromARGB(255, 0x75, 0x17, 0x1e),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Welcome to the UNI Workluster App',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            Text(
-              'Click the info icon to know more',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showInfo,
-        tooltip: 'Info',
-        backgroundColor: const Color.fromARGB(255, 0x75, 0x17, 0x1e),
-        child: const Icon(Icons.info_outline),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    return StoreProvider(
+      store: state,
+      child: MaterialApp(
+          title: 'uni',
+          theme: applicationLightTheme,
+          home: SplashScreen(),
+          navigatorKey: NavigationService.navigatorKey,
+          // ignore: missing_return
+          onGenerateRoute: (RouteSettings settings) {
+            switch (settings.name) {
+              case '/' + Constants.navPersonalArea:
+                return PageTransition.makePageTransition(
+                    page: HomePageView(), settings: settings);
+              case '/' + Constants.navSchedule:
+                return PageTransition.makePageTransition(
+                    page: SchedulePage(), settings: settings);
+              case '/' + Constants.navExams:
+                return PageTransition.makePageTransition(
+                    page: ExamsPageView(), settings: settings);
+              case '/' + Constants.navStops:
+                return PageTransition.makePageTransition(
+                    page: BusStopNextArrivalsPage(), settings: settings);
+              case '/' + Constants.navAbout:
+                return PageTransition.makePageTransition(
+                    page: AboutPageView(), settings: settings);
+              case '/' + Constants.navBugReport:
+                return PageTransition.makePageTransition(
+                    page: BugReportPageView(),
+                    settings: settings,
+                    maintainState: false);
+              case '/' + Constants.navLogOut:
+                return LogoutRoute.buildLogoutRoute();
+            }
+          }),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 60),
+        (Timer t) => state.dispatch(SetCurrentTimeAction(DateTime.now())));
   }
 }
