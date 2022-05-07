edfbb49d78c05564a4906f0f20b100f2288f40b1
frenato00 <up201908044@up.pt>
2022-05-07 19:10:08 +0100

    created group tab and empty page

--- src/lib/model/groups_model.dart
+++ src/lib/model/groups_model.dart
@@ -4,7 +4,10 @@ import 'package:uni/model/entities/lecture.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:uni/view/Pages/schedule_page_view.dart';
+import 'package:uni/view/Pages/groups_page_view.dart';
 import 'package:uni/view/Pages/secondary_page_view.dart';
+
+import 'entities/course.dart';
 
 class GroupsPage extends StatefulWidget {
   const GroupsPage({Key key}) : super(key: key);
@@ -16,60 +19,61 @@ class GroupsPage extends StatefulWidget {
 class _GroupsPageState extends SecondaryPageViewState
     with SingleTickerProviderStateMixin {
   final int weekDay = DateTime.now().weekday;
-
-  TabController tabController;
-  ScrollController scrollViewController;
-
-  final List<String> daysOfTheWeek = [
-    'Segunda-feira',
-    'Terça-feira',
-    'Quarta-feira',
-    'Quinta-feira',
-    'Sexta-feira'
-  ];
-
-  List<List<Lecture>> _groupLecturesByDay(schedule) {
-    final aggLectures = <List<Lecture>>[];
-
-    for (int i = 0; i < daysOfTheWeek.length; i++) {
-      final List<Lecture> lectures = <Lecture>[];
-      for (int j = 0; j < schedule.length; j++) {
-        if (schedule[j].day == i) lectures.add(schedule[j]);
-      }
-      aggLectures.add(lectures);
-    }
-    return aggLectures;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    tabController = TabController(vsync: this, length: daysOfTheWeek.length);
-    final offset = (weekDay > 5) ? 0 : (weekDay - 1) % daysOfTheWeek.length;
-    tabController.animateTo((tabController.index + offset));
-  }
-
-  @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget getBody(BuildContext context) {
-    return StoreConnector<AppState, Tuple2<List<Lecture>, RequestStatus>>(
-      converter: (store) => Tuple2(store.state.content['schedule'],
-          store.state.content['scheduleStatus']),
-      builder: (context, lectureData) {
-        final lectures = lectureData.item1;
-        final scheduleStatus = lectureData.item2;
-        return SchedulePageView(
-            tabController: tabController,
-            scrollViewController: scrollViewController,
-            daysOfTheWeek: daysOfTheWeek,
-            aggLectures: _groupLecturesByDay(lectures),
-            scheduleStatus: scheduleStatus);
-      },
-    );
-  }
+  //
+  // TabController tabController;
+  // ScrollController scrollViewController;
+  //
+  // List<String> daysOfTheWeek = [
+  //   'teste-feira',
+  //   'Terça-feira',
+  //   'Quarta-feira',
+  //   'Quinta-feira',
+  //   'Sexta-feira'
+  // ];
+  //
+  //
+  // List<List<Lecture>> _groupLecturesByDay(schedule) {
+  //   final aggLectures = <List<Lecture>>[];
+  //
+  //   for (int i = 0; i < daysOfTheWeek.length; i++) {
+  //     final List<Lecture> lectures = <Lecture>[];
+  //     for (int j = 0; j < schedule.length; j++) {
+  //       if (schedule[j].day == i) lectures.add(schedule[j]);
+  //     }
+  //     aggLectures.add(lectures);
+  //   }
+  //   return aggLectures;
+  // }
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   tabController = TabController(vsync: this, length: daysOfTheWeek.length);
+  //   final offset = (weekDay > 5) ? 0 : (weekDay - 1) % daysOfTheWeek.length;
+  //   tabController.animateTo((tabController.index + offset));
+  // }
+  //
+  // @override
+  // void dispose() {
+  //   tabController.dispose();
+  //   super.dispose();
+  // }
+  //
+  // @override
+  // Widget getBody(BuildContext context) {
+  //   return StoreConnector<AppState, Tuple2<List<Lecture>, RequestStatus>>(
+  //     converter: (store) => Tuple2(store.state.content['schedule'],
+  //         store.state.content['scheduleStatus']),
+  //     builder: (context, lectureData) {
+  //       final lectures = lectureData.item1;
+  //       final scheduleStatus = lectureData.item2;
+  //       return GroupsPageView(
+  //           tabController: tabController,
+  //           scrollViewController: scrollViewController,
+  //           daysOfTheWeek: daysOfTheWeek,
+  //           aggLectures: _groupLecturesByDay(lectures),
+  //           scheduleStatus: scheduleStatus);
+  //     },
+  //   );
+  // }
 }
